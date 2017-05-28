dd59f9b3a2552a0bfede8dce14fbe5538b83aa54
teawater <teawater@126.com>
2017-05-28 23:30:31 +0800

    Update

--- turtlet.py
+++ turtlet.py
@@ -9,26 +9,40 @@ class Application(Tkinter.Frame):
 		Tkinter.Frame.__init__(self, master)
 		self.pack()
 
-		self.cav = Tkinter.Canvas(self, bg = 'white', width = self.winfo_screenwidth()/2, height = self.winfo_screenheight()/2)
-		self.cav.pack()
-		#self.bind("<Enter>", self.left)
-		self.cav.bind_all("<KeyPress-Left>", self.left)
-		self.cav.bind_all("<KeyRelease-Left>", self.left_release)
+		width = self.winfo_screenwidth()
+		height = self.winfo_screenheight()
 
+		self.cav = Tkinter.Canvas(self, bg = 'white', width = width/3*2, height = height)
+		self.cav.pack(side = "left")
 		self.tus = turtle.TurtleScreen(self.cav)
 		self.tur = turtle.RawTurtle(self.tus)
-		self.tur.forward(10)
 
-	def left(self, event):
-		print 1
-		#self.cav.config(width = self.winfo_screenwidth(), height = self.winfo_screenheight())
-		#self.cav.config(width = 100)
-		self.tur.forward(10)
+		self.text = Tkinter.Text(self, width = width/3, height = height)
+		self.text.config(state = Tkinter.DISABLED)
+		self.text.pack(side = "right")
+		# self.code = Tkinter.StringVar()
+		# self.text["textvariable"] = self.code
+		self.text.insert(Tkinter.END, "Just a text Widget\nin two lines\n")
+		self.text.delete("2.0", Tkinter.END)
 
-	def left_release(self, event):
-		print 2
+		self.bind_all("<KeyPress-Up>", self.get_key)
+		self.bind_all("<KeyPress-Down>", self.get_key)
+		self.bind_all("<KeyPress-Left>", self.get_key)
+		self.bind_all("<KeyPress-Right>", self.get_key)
+
+	def get_key(self, event):
+		if event.keysym == "Up":
+			self.tur.forward(5)
+		elif event.keysym == "Down":
+			self.tur.backward(5)
+		elif event.keysym == "Left":
+			self.tur.left(5)
+		elif event.keysym == "Right":
+			self.tur.right(5)
 
 if __name__ == '__main__':
-	root = Tkinter.Tk()	
+	root = Tkinter.Tk()
+	root.wm_state('zoomed')
+	root.resizable(0,0)
 	app = Application(master=root)
 	app.mainloop()
